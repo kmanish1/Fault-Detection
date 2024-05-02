b89eef277805134b7d78e42d3fe02aba6499279a
kmanish1 <[kmanishreddy11@gmail.com]>
2024-05-02 18:37:39 +0530

    made ui

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [ia, setia]=useState(0)
   const [ib, setib]=useState(0)
   const [ic, setic]=useState(0)
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState("hello bro chandu radhe radhe radhe shyam");
 
   async function handleSubmit(event){
     event.preventDefault();
@@ -42,52 +42,53 @@ function App() {
   }
 
   return (
-    <div className="bg-gray-600 flex flex-col justify-center items-center h-screen">
-      <h1>Fault Detection in power lines using Machine Learning Algorithms</h1>
-      <form onSubmit={handleSubmit} className=" h-screen flex flex-row ">
-        <div class="bg-red-200 border-red-600 border-8">
-          <div class="">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+    <div className="bg-[rgb(15,23,42)] h-screen text-slate-300">
+      <h1 className="font-mullish font-bold text-2xl text-center pt-5">Fault Detection in power lines using Machine Learning Algorithms</h1>
+      <form onSubmit={handleSubmit} className="flex flex-row justify-center items-center">
+        <div class="m-5 p-5 flex flex-col flex-wrap ">
+          <div class="p-2">
+            <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="grid-first-name">
               Enter Voltage V1
             </label>
-            <input type="number" onChange={(e) => setVa(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name"   />
+            <input type="number" onChange={(e) => setVa(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name"   />
           </div>
-          <div class="">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+          <div class="p-2">
+            <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="grid-last-name">
               Enter Voltage V2
             </label>
             <input type="number" onChange={(e) => setVb(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" />
           </div>
-          <div class="">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+          <div class="p-2">
+            <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="grid-last-name">
               Enter Voltage V3
             </label>
             <input type="number" onChange={(e) => setVc(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" />
           </div>
         </div>
-        <div class="bg-white border-red-200 border-8 ">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        <div class="m-5 p-5 flex flex-col flex-wrap">
+          <div class="p-2">
+            <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="grid-first-name">
               Enter Current I1
             </label>
-            <input type="number" onChange={(e) => setia(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name"   />
+            <input type="number" onChange={(e) => setia(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name"   />
           </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+          <div class="p-2">
+            <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="grid-last-name">
               Enter Current I2
             </label>
             <input type="number" onChange={(e) => setib(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" />
           </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+          <div class="p-2">
+            <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="grid-last-name">
               Enter Current I3
             </label>
             <input type="number" onChange={(e) => setic(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" />
           </div>
         </div>
-        <button type="submit">Submit</button>
       </form>
-      <h1>{result}</h1>
+      <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 btn bg-sky-500/50 p-3 rounded-lg block font-bold text-center mx-auto" type="submit">Submit</button>
+      <h1 className="block font-semibold text-center mx-auto m-3 p-3">{result}</h1>
+      
     </div>
   )
 }
